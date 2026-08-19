ebfe4dd712131c05e0d4e86e1f1dc4f700ecf9cf
Kushagra Sharma <kushagra.sharma595@gmail.com>
2026-08-19 11:27:16 +0530

    Added Source C Files

---
+++ Lab2/Q1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm, i;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    if (n == 0) {
+    } else if (n == 1) {
+        printf("%d", t1);
+    } else {
+        printf("%d, %d", t1, t2);
+
+        for (i = 3; i <= n; ++i) {
+            nextTerm = t1 + t2;
+            printf(", %d", nextTerm);
+            t1 = t2;
+            t2 = nextTerm;
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
