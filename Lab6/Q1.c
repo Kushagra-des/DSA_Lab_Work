baa9c7baaa495a98c6aec89e033e9ecbf33c70d1
Kushagra Sharma <kushagra.sharma595@gmail.com>
2026-09-04 09:53:22 +0530

    Added Source C File

---
+++ Lab6/Q1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+struct Node* Insert_Beginning(struct Node *head, int value) {
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = head;
+    return newNode;
+}
+
+void Display(struct Node *head) {
+    struct Node *temp = head;
+    if (temp == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    printf("Linked List: ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node *head = NULL;
+    int n, value;
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter data for node %d: ", i + 1);
+        scanf("%d", &value);
+        head = Insert_Beginning(head, value);
+    }
+
+    Display(head);
+    return 0;
+}
