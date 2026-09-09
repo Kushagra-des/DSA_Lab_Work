68b49fc141538f538891aaa8fdbc9cab5f26e312
Kushagra Sharma <kushagra.sharma595@gmail.com>
2026-09-09 20:31:44 +0530

    Added Source C File

---
+++ Lab8/Q4.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char x) {
+    stack[++top] = x;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+int priority(char x) {
+    if (x == '(') return 0;
+    if (x == '+' || x == '-') return 1;
+    if (x == '*' || x == '/') return 2;
+    return 0;
+}
+
+int main() {
+    char exp[MAX], *e, x;
+    printf("Enter Infix Expression: ");
+    scanf("%s", exp);
+    e = exp;
+    printf("Postfix Expression: ");
+    while (*e != '\0') {
+        if (isalnum(*e))
+            printf("%c", *e);
+        else if (*e == '(')
+            push(*e);
+        else if (*e == ')') {
+            while ((x = pop()) != '(')
+                printf("%c", x);
+        } else {
+            while (top != -1 && priority(stack[top]) >= priority(*e))
+                printf("%c", pop());
+            push(*e);
+        }
+        e++;
+    }
+    while (top != -1)
+        printf("%c", pop());
+    printf("\n");
+    return 0;
+}
