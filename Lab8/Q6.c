f85e26138593caec7080642f4bde241baa8a8060
Kushagra Sharma <kushagra.sharma595@gmail.com>
2026-09-15 22:25:38 +0530

    Added Source C File

---
+++ Lab8/Q6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+int stack[MAX];
+int top = -1;
+
+void push(int x) { stack[++top] = x; }
+int pop() { return stack[top--]; }
+
+int main() {
+    char exp[MAX];
+    int i;
+    printf("Enter Postfix Expression: ");
+    scanf("%s", exp);
+
+    for (i = 0; exp[i] != '\0'; i++) {
+        if (isdigit(exp[i]))
+            push(exp[i] - '0');
+        else {
+            int val2 = pop();
+            int val1 = pop();
+            switch (exp[i]) {
+                case '+': push(val1 + val2); break;
+                case '-': push(val1 - val2); break;
+                case '*': push(val1 * val2); break;
+                case '/': push(val1 / val2); break;
+            }
+        }
+    }
+    printf("Result: %d\n", pop());
+    return 0;
+}
